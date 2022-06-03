623940bc44e4bb439beff03d1bf9eb51ce806bf5
Manya7864 <manishyadav78642080@gmail.com>
2022-06-03 11:45:21 +0530

    creditcard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from '../src/Component/Form'
+import CreditCard from "../src/Component/CreditCard"
+import { useState } from 'react';
+
+const initialInfo = {
+  cardHolder: "",
+  cardNumber:"",
+  expMonth: "",
+  expYear: "",
+  cvc: ""
+}
 
 function App() {
+  const [info, setInfo] = useState(initialInfo)
+
+  const handleChange = ((e)=>setInfo((prev)=>({...prev,[e.target.name]:e.target.value})))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreditCard info={info} />
+      <Form info={info} setInfo={setInfo} handleChange={handleChange} />
     </div>
   );
 }
